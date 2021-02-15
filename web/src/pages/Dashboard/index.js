fb93d38b502e93679d5a6f6a604144e2ccba9ac3
Mateus Kunrath <mateus9g.dev@gmail.com>
2021-02-14 21:50:45 -0300

    Inicio da configuracao do projeto

---
+++ web/src/pages/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import Header from '../../components/Header';
+
+function Dashboard() {
+  return (
+    <Container>
+      <Header />
+      <div />
+    </Container>
+  );
+}
+
+export default Dashboard;
